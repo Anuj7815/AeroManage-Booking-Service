a4cbda4f0048f0518cd994af667d587988ad9f2e
Anshul Bansal <anshul@Anshuls-MacBook-Air.local>
2025-04-19 14:28:01 +0530

    Base template added

---
+++ src/index.js
@@ -0,0 +1,11 @@
+const express=require('express');
+const app=express();
+const {ServerConfig, Logger}=require('./config');
+const apiRoutes=require('./routes');
+
+app.use('/api',apiRoutes);
+
+app.listen(ServerConfig.PORT,()=>{
+    console.log("Server is Running on PORT: ",ServerConfig.PORT);
+});
+
