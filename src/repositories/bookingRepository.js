0222826b3007233febde0605b82e8af76daf7331
Anshul Bansal <anshul@Anshuls-MacBook-Air.local>
2025-04-21 17:34:42 +0530

    Boiler plate of Booking added

---
+++ src/repositories/bookingRepository.js
@@ -0,0 +1,11 @@
+const {StatusCodes}=require('http-status-codes');
+const {Booking}=require('../models');
+const CrudRepository=require('./crudRepository');
+
+class BookingRepository extends CrudRepository{
+    constructor(){
+        super(Booking);
+    }
+}
+
+module.exports=BookingRepository;
